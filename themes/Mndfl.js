34d78391acd4e990c2525842b83feb3e237976bc
Jake Elder <jake@jakeelder.info>
2021-02-14 11:18:31 +0700

    Adding Mndfl theme

---
+++ themes/Mndfl.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+  "#000000",
+  "#cd3c3c",
+  "#75c478",
+  "#eed961",
+  "#62d4ff",
+  "#c397d8",
+  "#53c2bd",
+  "#ffffff",
+  "#999999",
+  "#ffa5a5",
+  "#9ae99d",
+  "#f8ea93",
+  "#b6d8f9",
+  "#b77ee0",
+  "#54ced6",
+  "#ffffff",
+]);
+t.prefs_.set("foreground-color", "#ffffff");
+t.prefs_.set("background-color", "#222222");
+t.prefs_.set("cursor-color", "rgba(255,255,29,0.5)");
